c417d59ab725dac238939ccfdb78efc1af815e48
Juan Jose Castro <juanjosecastrogutierrez@gmail.com>
2026-01-20 18:57:01 -0500

    first commit

---
+++ src/main/java/dev/juancastro/otel/HomeController.java
@@ -0,0 +1,55 @@
+package dev.juancastro.otel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+
+@RestController
+public class HomeController {
+
+    private final ObservationRegistry observationRegistry;
+
+    public HomeController(ObservationRegistry observationRegistry) {
+        this.observationRegistry = observationRegistry;
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/")
+    public String home() {
+
+        Observation.createNotStarted("home.counter", observationRegistry).observe(() -> {
+            log.info("Home endpoint called");
+        });
+        return "Hello World!";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable("name") String name) {
+        log.info("Greeting user: {}", name);
+        simulateWork();
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/slow")
+    public String slow() throws InterruptedException {
+        log.info("Starting slow operation");
+        Thread.sleep(500);
+        log.info("Slow operation completed");
+        return "Done!";
+    }
+
+    private void simulateWork() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
